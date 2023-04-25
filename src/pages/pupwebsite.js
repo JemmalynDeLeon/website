b528771a5c277012b058d6546dd21d636eec08bb
Jemma De Leon <jemmaadeleon@gmail.com>
2023-04-26 07:02:17 +0800

    footnote and navbar initial commit

---
+++ src/pages/pupwebsite.js
@@ -0,0 +1,30 @@
+import Navbar from '@components/navbar2.js'
+import Footer from '@components/footnote.js'
+import Navbar2 from '@/components/navbar'
+
+
+
+export default function home() {
+  return (
+    <>
+      <Navbar />
+      <Navbar2 />
+     
+
+      <section class="py-16 px-16 flex-col gap-6 pl-10">
+
+        <iframe class="flex justify-center" width="750" height="422" src="https://www.youtube.com/embed/EVp6JBwTudk?list=UU3D3_jrSi3sYmx5L5p5RAwA" title="The Observer Online: Tanglaw Fest 2023" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe class="flex justify-center" style={{ borderRadius: '12px' }} src="https://open.spotify.com/embed/episode/6hSNhWq3gSGcSouucc8kl0?utm_source=generator" width="750" height="280" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+
+      </section>
+
+      <section class='bg-gray'>
+
+        <Footer />
+
+      </section>
+    </>
+
+  )
+}
