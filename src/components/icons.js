303e8ab5f102fc4e3888ee22acd6dbe387db0456
Jemma De Leon <jemmaadeleon@gmail.com>
2023-04-27 04:06:24 +0800

    pup website

--- src/components/icons.js
+++ src/components/icons.js
@@ -2,22 +2,7 @@
 
 function Icons() {
     return (  <div></div>
-    // <div className="flex flex-col p-10 ">
-    // <div  class="flex flex-row gap-7">
-    // <span class="inline-flex items-center justify-center h-12 w-12 rounded-full bg-red hover:bg-blue">
-    //             <i class="fa fa-brands fa-facebook text-white text-lg"></i></span>
-    //         <span class="inline-flex items-center justify-center h-12 w-12 rounded-full bg-red hover:bg-blue">
-    //             <i class="fa fa-brands fa-twitter text-white"></i></span>
-    //         <span class="inline-flex items-center justify-center h-12 w-12 rounded-full bg-red hover:bg-blue">
-    //             <i class=" fa fa-brands fa-youtube text-white"></i></span>
-    //         <span class="inline-flex items-center justify-center h-12 w-12 rounded-full bg-red hover:bg-blue">
-    //             <i class="fa fa-brands fa-linkedin text-white"></i></span>
-    //         <span class="inline-flex items-center justify-center h-12 w-12 rounded-full bg-red hover:bg-blue">
-    //             <i class="fa fa-solid fa-rss text-white"></i></span>
-    //         <span class="inline-flex items-center justify-center h-12 w-12 rounded-full bg-red hover:bg-blue">
-    //             <i class="fa fa-brands fa-spotify text-white "></i></span>
-    // </div>
-    // </div>
+ 
     );
     }
 export default Icons;
