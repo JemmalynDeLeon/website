46bb9dcb505186eaa7faeac9a097143404d0d7fb
Jemma De Leon <jemmaadeleon@gmail.com>
2023-04-26 22:41:20 +0800

    added tailwind config

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default function home() {
 
         <iframe src="https://www.youtube.com/embed/EVp6JBwTudk?list=UU3D3_jrSi3sYmx5L5p5RAwA" width="50%" height="450" allowfullscreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"loading="lazy"/>
         <iframe src="https://open.spotify.com/embed/episode/6hSNhWq3gSGcSouucc8kl0?utm_source=generator" width="50%"height="352"allowfullscreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"/>
-        {/* <blockquote class="twitter-tweet" class="flex flex-col space-y-4 items-start justify-start m-6"><p lang="en" dir="ltr">Mid-Year Commencement Exercises Schedule 🎓🎓🎓 <a href="https://t.co/QAlrfrH6bJ">pic.twitter.com/QAlrfrH6bJ</a></p>&mdash; PUP (Official) (@ThePUPOfficial) <a href="https://twitter.com/ThePUPOfficial/status/1648137053908135936?ref_src=twsrc%5Etfw">April 18, 2023</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" ></script> */}
+        <blockquote class="twitter-tweet"><p lang="en" dir="ltr">Mid-Year Commencement Exercises Schedule 🎓🎓🎓 <a href="https://t.co/QAlrfrH6bJ">pic.twitter.com/QAlrfrH6bJ</a></p>&mdash; PUP (Official) (@ThePUPOfficial) <a href="https://twitter.com/ThePUPOfficial/status/1648137053908135936?ref_src=twsrc%5Etfw">April 18, 2023</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" ></script>
       </section>
 
       <section class="bg-white-700">
