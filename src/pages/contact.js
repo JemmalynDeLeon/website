9eb6f7adc999bcf8a225a1dde5e160d72902d09f
Jemma De Leon <jemmaadeleon@gmail.com>
2023-05-11 03:12:59 +0800

    edit nav 2

---
+++ src/pages/contact.js
@@ -0,0 +1,23 @@
+import Navbar2 from '@/components/nav2.js'
+import Footnote from '@/components/footnote.js'
+import Navbar1 from '@/components/nav1'
+import Fab from '@/components/fab'
+import Contactus from '@/components/contactus'
+import Footend from '@/components/footend'
+
+const Contact = () => {
+
+    return (
+      <>
+        <Navbar2/>
+        <Navbar1/>
+        <Contactus/>
+        <Footnote/>
+        <Fab/>
+        <Footend/>
+      </>
+      
+    )
+  }
+  
+  export default Contact
